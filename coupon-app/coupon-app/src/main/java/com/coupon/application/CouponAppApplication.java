9558d7d2a1108c71eeadfc2088d9e86d677dccd5
nikhil thengane <nikhilthengane700@gmail.com>
2023-08-19 08:57:48 +0530

    Coupon System

--- coupon-app/coupon-app/src/main/java/com/coupon/application/CouponAppApplication.java
+++ coupon-app/coupon-app/src/main/java/com/coupon/application/CouponAppApplication.java
@@ -1,9 +1,13 @@
 package com.coupon.application;
 
+import com.coupon.application.service.CouponService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CouponAppApplication implements ApplicationRunner {
@@ -11,9 +15,19 @@ public class CouponAppApplication implements ApplicationRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CouponAppApplication.class, args);
 	}
+	@Autowired
+	private CouponService couponService;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		System.out.println("Hello");
+		System.out.println("Hello World");
+		String newCoupon = couponService.getNewCoupon();
+		System.out.println(newCoupon);
+		List<String> couponsList = couponService.getNewCoupon(100);
+		for(String coupon: couponsList){
+			System.out.println(coupon);
+		}
+
+
 	}
 }
